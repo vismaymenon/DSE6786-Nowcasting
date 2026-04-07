47a4c33310b4b22eeb8489173189837daa0daeae
jillianloh <136660798+jillianloh@users.noreply.github.com>
2026-04-08 00:45:48 +0800

    reversed my prev commit

--- supabase_client.py
+++ supabase_client.py
@@ -3,4 +3,4 @@ from dotenv import load_dotenv
 from supabase import create_client
 
 load_dotenv()
-supabase = create_client(os.getenv("https://uhxliyubyjwtpxjcfedk.supabase.co"), os.getenv("sb_publishable_tGsiADOfozJKrf5npbdyew_lS4y4otL"))
+supabase = create_client(os.getenv("PROJECT_URL"), os.getenv("PROJECT_KEY"))
